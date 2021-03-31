d5c177f1677e9c46bd9536e97be4897ca170de81
ssujinn <60187862+ssujinn@users.noreply.github.com>
2021-04-01 07:20:09 +0900

    Add Hello Wolrd 4 in team member A (#2)
    Co-authored-by: b-chae <cbr5964@naver.com>

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ print("Hello World 1 in team leader server")
 print("Hello Wolrd 2 in team leader server")
 
 print("Hello World 3 in team member B local server")
+
+print("Hello World 4 in team member A local server")
