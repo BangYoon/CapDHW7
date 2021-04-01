d0c2bea8ea57f257d4ce788f2fa3c2f4330fd896
b-chae <cbr5964@naver.com>
2021-04-01 14:43:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Hello World 1 in team leader server")
 
-print("Hello Wolrd 2 in team leader server")
+print("Hello World 2 in team leader server")
 
 print("Hello World 3 in team member B local server")
 
